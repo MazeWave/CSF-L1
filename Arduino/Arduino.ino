33d2fe41b0bd5c4dcedce2f4ed445adeb8294310
kryonor <92310966+kryonor@users.noreply.github.com>
2022-05-25 16:41:04 +0200

    Update Arduino.ino
    Set up HC-SR04.
    Started implementing LoRaWAN

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -6,35 +6,84 @@ TODO
 - Intégrer la partie LoRa après tests du module MFRC.
 **/
 
+#define CFG_EU 1
+
+#include <lmic.h>
+#include <hal/hal.h>
 #include <SPI.h>
-#include <MFRC522.h>
 
-#define RST_PIN         9           // Configurable, see typical pin layout above
-#define SS_PIN          10          // Configurable, see typical pin layout above
+// LoRaWAN end-device
+static const u4_t DEVADDR = 0x00000000;
+static const PROGMEM u1_t NWKSKEY[16] = {  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u1_t PROGMEM APPSKEY[16] = {  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static uint8_t mydata[30];
+static osjob_t sendjob;
+const unsigned TX_INTERVAL = 30;
+
+#define echoPin 2
+#define trigPin 3
+
+long duration;
+int distance;
 
 void setup()
 {
-  // TODO
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-  byte mfblock;
-  byte mflen;
-  MFRC522::StatusCode mfstatus;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-  //-------------------------------------------
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
 
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 }
+
+/*
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN  10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial);
+  SPI.begin();
+  rfid.PCD_Init();
+  delay(4);
+  rfid.PCD_DumpVersionToSerial();
+}
+
+void loop()
+{
+  if ( ! rfid.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  if ( ! rfid.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  rfid.PICC_DumpToSerial(&(rfid.uid));
+}
+*/
