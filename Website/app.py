addfb5ac993eae635737249eac5bbc1fec67b14c
kryonor <92310966+kryonor@users.noreply.github.com>
2022-05-29 12:58:17 +0200

    Major update
    Set up flask server, fixed implemented all pages.
    TODO fix tab icon

--- Website/app.py
+++ Website/app.py
@@ -1,8 +1,20 @@
-from flask import Flask
 from flask import *
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 
 @app.route("/")
-def index():
-    return(render_template('home.html'))
+def get_index():
+    return render_template('web.html', page="Home")
+
+@app.route("/stats")
+def get_stats():
+    return render_template('web.html', page='Stats')
+
+@app.route("/logs")
+def get_console():
+    return render_template('web.html', page='Logs')
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=3000, debug=True)
+
